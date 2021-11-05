f82fdaa53c3048e064306493e339506a3cb8c99c
Vladimir Bun <qwelp@mail.ru>
2021-11-05 09:58:16 +0300

    Fix brain-progression

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -37,11 +37,15 @@ function start(): void
     }
 }
 
-function IsPrime($p)
+function IsPrime($num)
 {
-    $r1 = $p % 2;
-    $r2 = $p % 3;
-    $r3 = $p % 5;
+    $flag = true;
+    for ($i = 2; $i < $num; $i++) {
+        if ($num % $i === 0) {
+            $flag = false;
+            break;
+        }
+    }
 
-    return ($p > 1) && (($r1 >= 1) && ($r2 >= 1) && ($r3 >= 1)) || in_array($p, [2,3,5]);
+    return $flag;
 }
