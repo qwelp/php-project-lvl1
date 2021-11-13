b522b24e9090a912213b9c80b3330bb86ab6c600
Vladimir Bun <qwelp@mail.ru>
2021-11-13 15:43:08 +0300

    Fix

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -6,10 +6,10 @@ use function Src\Engine\startGame;
 
 use const Src\Engine\{RANDOM_MIN, RANDOM_MAX};
 
-function evenGame()
+function evenGame(): void
 {
     $questionMain = 'Answer "yes" if the number is even, otherwise answer "no".';
-    $arParamsFunc = function () {
+    $arParamsFunc = function (): callable {
         $randInt = random_int(RANDOM_MIN, RANDOM_MAX);
         $result = ($randInt % 2 == 0) ? 'yes' : 'no';
 
